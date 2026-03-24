bae9043c88f9ad387d30b9e64f1b6b815716eca8
Bünyamin Genç <bnymgncc@gmail.com>
2026-03-24 20:19:15 +0300

    FIX: CORS sıralaması

--- core/settings.py
+++ core/settings.py
@@ -29,13 +29,14 @@ INSTALLED_APPS = [
 ]
 
 
+# --- MIDDLEWARE (Sıralama Önemlidir) ---
 MIDDLEWARE = [
-    'corsheaders.middleware.CorsMiddleware', # İŞTE SİHİRLİ DOKUNUŞ: BUNU EN ÜSTE ALDIK!
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware', 
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',s
+    'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
